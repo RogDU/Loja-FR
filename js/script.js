d01d67d925804b976d6d6100e135c0e174ded019
Rogério Duarte Xavier <rogdu2@gmail.com>
2026-01-29 12:54:56 -0300

    correções

--- js/script.js
+++ js/script.js
@@ -1,39 +1,42 @@
-console.log('FunkoRogs')
 
-const getProducts = async() => {
+//const formatCurrency = (number) => {
+//  return number.toLocaleString('pt-BR', {
+//    style: 'currency',
+//  currency: 'BRL'
+//})
+//} //ou
+const numberFormat = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+    maximumFractionDigits: 2,
+    maximumFractionDigits: 2,
+})
+const getProducts = async () => {
     const response = await fetch('js/products.json')
-    const data = await response.json()    
+    const data = await response.json()
     return data
 }
-
-const generateCard = async ()=> {
-
-    const products = await getProducts()    
-
+const generateCard = async () => {
+    const products = await getProducts()
     products.map(product => {
         //console.log(product)
         let card = document.createElement('div')
         card.classList.add('card__produto')
         card.innerHTML = `
          <figure>
-
              <img src="images/${product.image}" alt="${product.product_name}" />
          </figure>
-
-
         <div class="card__produto_detalhes">
             <h4>${product.product_name}</h4>
             <h5>${product.product_model}</h5>
                     </div>
 
-            <h6>R$ ${product.price}</h6>
+            <h6>${numberFormat.format(product.price)}</h6>
        `
 
-       const listaProdutos = document.querySelector('.lista__produtos')
-       listaProdutos.appendChild(card)
+        const listaProdutos = document.querySelector('.lista__produtos')
+        listaProdutos.appendChild(card)
     })
-
 }
-
 
 generateCard()
