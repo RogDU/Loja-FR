2ca69b9a956e55871d469d903a3f977db01c6d1a
Rogério Duarte Xavier <rogdu2@gmail.com>
2026-02-02 10:52:46 -0300

    alteração

--- js/script.js
+++ js/script.js
@@ -5,38 +5,49 @@
 //  currency: 'BRL'
 //})
 //} //ou
-const numberFormat = new Intl.NumberFormat('pt-BR', {
-    style: 'currency',
-    currency: 'BRL',
-    maximumFractionDigits: 2,
-    maximumFractionDigits: 2,
-})
+
+console.log('Jordan Shoes')
+
+const formatCurrency = (number) => {
+  return number.toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+    })
+}
+
 const getProducts = async () => {
     const response = await fetch('js/products.json')
     const data = await response.json()
     return data
 }
+
 const generateCard = async () => {
+
     const products = await getProducts()
+
     products.map(product => {
-        //console.log(product)
+
         let card = document.createElement('div')
         card.classList.add('card__produto')
+        
         card.innerHTML = `
-         <figure>
-             <img src="images/${product.image}" alt="${product.product_name}" />
-         </figure>
+        <figure>
+            <img src="images/${product.image}" alt="${product.product_name}" />
+        </figure>
+
         <div class="card__produto_detalhes">
             <h4>${product.product_name}</h4>
             <h5>${product.product_model}</h5>
-                    </div>
+        </div>
 
-            <h6>${numberFormat.format(product.price)}</h6>
-       `
+        <h6>${formatCurrency(product.price)}</h6>
+        `
 
         const listaProdutos = document.querySelector('.lista__produtos')
         listaProdutos.appendChild(card)
+
     })
+   
 }
 
 generateCard()
